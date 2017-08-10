5643c8e1e16945dafe1ca47f8172b06aabe382ea
jose-coto <gerrall@hotmail.com>
2017-08-09 23:10:52 -0600

    Primer commit subida del proyecto

---
+++ app/src/main/java/cr/ac/ucr/app_proyecto_i_aplicada/MainActivity.java
@@ -0,0 +1,13 @@
+package cr.ac.ucr.app_proyecto_i_aplicada;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
